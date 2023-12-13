7708110a663ba60493c9100ca32a23670ee648c9
MicoFeb <sutanmico@gmail.com>
2023-12-13 14:36:35 +0700

    update: test 1

--- Test_1/func.js
+++ Test_1/func.js
@@ -1,19 +1,25 @@
+// Rumus A000124 of Sloane’s OEIS yang saya dapat dari internet yaitu = a(n) = n * (n + 1)/2
+
+//Rumus
 const calculateNum = (n) => {
 	return (n * (n + 1)) / 2;
 };
 
+// Generate Output
 const generate = (length) => {
-	let sequence = [];
+	let output = [];
 
 	for (let i = 1; i <= length; i++) {
 		let num = calculateNum(i);
+
+		// Karena di contoh output pada soal, output asli dikurangi 1, 2, 3, ... maka:
 		if (i > 1) {
 			num -= i - 1;
 		}
-		sequence.push(num);
+		output.push(num);
 	}
 
-	return sequence.join('-');
+	return output.join('-');
 };
 
 module.exports = generate;
--- Test_1/main.js
+++ Test_1/main.js
@@ -1,4 +1,4 @@
-const generate = require('./func'); // Pastikan path-nya sudah sesuai
+const generate = require('./func');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -6,9 +6,21 @@ const rl = readline.createInterface({
 	output: process.stdout,
 });
 
-rl.question('Masukkan panjang deret: ', (num) => {
-	const input = parseInt(num);
-	const output = generate(input);
-	console.log(`Output: ${output}`);
-	rl.close();
-});
+const getInput = () => {
+	rl.question('Input panjang deret: ', (num) => {
+		const input = parseInt(num);
+		const output = generate(input);
+		console.log(`Output: ${output}`);
+
+		// Untuk looping
+		rl.question('Ingin mengulang? (y/n): ', (again) => {
+			if (again.toLowerCase() === 'y') {
+				getInput();
+			} else {
+				rl.close();
+			}
+		});
+	});
+};
+
+getInput();
